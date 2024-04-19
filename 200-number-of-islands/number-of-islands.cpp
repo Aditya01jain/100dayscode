b2ca8ba41c55d3abfb89799f19268ede8eab977e
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-04-19 18:52:24 +0530

    Time: 15 ms (99.69%) | Memory: 15.7 MB (94.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,37 +1,39 @@
+auto init = []() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return 'c';
+}();
+
 class Solution {
- public:
-  int numIslands(vector<vector<char>>& grid) {
-    constexpr int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    const int m = grid.size();
-    const int n = grid[0].size();
-    int ans = 0;
+    void removeIsland(short int i, short int j, vector<vector<char>> &grid)
+    {
+      short int m = grid.size(), n = grid[0].size();
+      if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0')
+        return;
 
-    auto bfs = [&](int r, int c) {
-      queue<pair<int, int>> q{{{r, c}}};
-      grid[r][c] = '2';  // Mark '2' as visited.
-      while (!q.empty()) {
-        const auto [i, j] = q.front();
-        q.pop();
-        for (const auto& [dx, dy] : dirs) {
-          const int x = i + dx;
-          const int y = j + dy;
-          if (x < 0 || x == m || y < 0 || y == n)
-            continue;
-          if (grid[x][y] != '1')
-            continue;
-          q.emplace(x, y);
-          grid[x][y] = '2';  // Mark '2' as visited.
-        }
-      }
-    };
+      grid[i][j] = '0';
+      removeIsland(i, j + 1, grid);
+      removeIsland(i + 1, j, grid);
+      removeIsland(i, j - 1, grid);
+      removeIsland(i - 1, j, grid);
+    }
+public:
+    short int numIslands(vector<vector<char>>& grid) {
+        short int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
 
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (grid[i][j] == '1') {
-          bfs(i, j);
-          ++ans;
+        for (short int i = 0; i < m; i++)
+        {
+          for (short int j = 0; j < n; j++)
+          {
+            if (grid[i][j] == '1')
+            {
+              islands++;
+              removeIsland(i, j, grid);
+            }
+          }
         }
 
-    return ans;
-  }
+        return islands;
+    }
 };
