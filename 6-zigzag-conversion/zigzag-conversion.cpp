bd6c1ffe41617258b6a4aeca268d429a4410dab7
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-04-21 16:40:34 +0530

    Time: 10 ms (58.54%) | Memory: 13.4 MB (56.57%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,27 +1,23 @@
 class Solution {
  public:
- string convert(string s, int numRows) {
-    if (numRows == 1 || numRows >= s.length()) {
-        return s;
-    }
-    vector<string> rows(numRows, "");
-    
-    cout<<endl;
-    int index = 0;
-    bool goingDown = false;
-    for (char c : s) {
-        rows[index] += c;
-        if (index == 0 || index == numRows - 1) {
-            goingDown = !goingDown;
+string convert(std::string s, int numRows) {
+        if (numRows == 1 || numRows >= s.size()) {
+            return s;
         }
-        index += goingDown ? 1 : -1;
+        int index = 0;
+        string ans = "";
+        vector<string> rows(numRows, "");
+        bool down = false;
+        for (char c : s) {
+            rows[index] += c;
+            if (index == 0 || index == numRows - 1) {
+                down = !down;
+            }
+            index += down ? 1 : -1;
+        }
+        for (const auto &row : rows) {
+            ans += row;
+        }
+        return ans;
     }
-    string result;
-    for (string row : rows) {
-        result += row;
-    }
-    
-    return result;
-    
-}
 };
