2b82a28c05640a52b1d014ad385952be108d583b
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-05-01 22:32:59 +0530

    Time: 56 ms (22.97%) | Memory: 50.8 MB (50.44%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,11 +1,17 @@
+#include <vector>
+#include <algorithm> // for std::max
+
 class Solution {
- public:
-  bool canJump(vector<int>& nums) {
-    int i = 0;
-
-    for (int reach = 0; i < nums.size() && i <= reach; ++i)
-      reach = max(reach, i + nums[i]);
-
-    return i == nums.size();
-  }
+public:
+    bool canJump(std::vector<int>& nums) {
+        int reach = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (reach < i) // Can't reach the current position 'i'
+                return false;
+            reach = std::max(reach, i + nums[i]);
+            if (reach >= nums.size() - 1) // Can reach the last index
+                return true;
+        }
+        return true; // This is unreachable if the last index can't be reached.
+    }
 };
