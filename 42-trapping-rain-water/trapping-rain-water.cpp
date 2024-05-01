e6ebfc43d019a9962db873c191ced8fa5b09ce01
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-05-02 00:11:57 +0530

    Time: 7 ms (87.78%) | Memory: 23.2 MB (13.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,20 +1,37 @@
+#include <vector>
+#include <algorithm> // for std::max
+
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        const int n = height.size();
-    int ans = 0;
-    vector<int> l(n);  // l[i] := max(height[0..i])
-    vector<int> r(n);  // r[i] := max(height[i..n))
-
-    for (int i = 0; i < n; ++i)
-      l[i] = i == 0 ? height[i] : max(height[i], l[i - 1]);
-
-    for (int i = n - 1; i >= 0; --i)
-      r[i] = i == n - 1 ? height[i] : max(height[i], r[i + 1]);
-
-    for (int i = 0; i < n; ++i)
-      ans += min(l[i], r[i]) - height[i];
-
-    return ans;
+    void leftmax(std::vector<int>& height, std::vector<int>& left) {
+        int m = -1;
+        for (int i = 0; i < height.size(); i++) {
+            m = std::max(m, height[i]);
+            left[i] = m;
+        }
+    }
+    
+    void rightmax(std::vector<int>& height, std::vector<int>& right) {
+        int m = -1;
+        for (int i = height.size() - 1; i >= 0; i--) {
+            m = std::max(m, height[i]);
+            right[i] = m;
+        }
+    }
+    
+    int trap(std::vector<int>& height) {
+        int n = height.size();
+        int ans = 0;
+        std::vector<int> left(n);  // l[i] := max(height[0..i])
+        std::vector<int> right(n); // r[i] := max(height[i..n-1])
+        rightmax(height, right);
+        leftmax(height, left);
+        for (int i = 0; i < n; ++i) {
+            int temp = std::min(right[i], left[i]);
+            if (temp > height[i]) {
+                ans += temp - height[i];
+            }
+        }
+        return ans;
     }
 };
