6cb8085b4e45fe5b42c0280c365c31ba31a8e97e
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-05-02 00:12:47 +0530

    Time: 13 ms (34.89%) | Memory: 23.2 MB (29.15%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,37 +1,32 @@
-#include <vector>
-#include <algorithm> // for std::max
-
 class Solution {
 public:
-    void leftmax(std::vector<int>& height, std::vector<int>& left) {
-        int m = -1;
-        for (int i = 0; i < height.size(); i++) {
-            m = std::max(m, height[i]);
-            left[i] = m;
+    void leftmax(vector<int>& height,vector<int>& left){
+        int m =-1;
+        for(int i=0;i<height.size();i++){
+            m= max(m,height[i]);
+            left[i]=m;
         }
     }
-    
-    void rightmax(std::vector<int>& height, std::vector<int>& right) {
-        int m = -1;
-        for (int i = height.size() - 1; i >= 0; i--) {
-            m = std::max(m, height[i]);
-            right[i] = m;
+    void rightmax(vector<int>& height,vector<int>& right){
+        int m =-1;
+        for(int i=height.size()-1;i>=0;i--){
+            m= max(m,height[i]);
+            right[i]=m;
         }
     }
-    
-    int trap(std::vector<int>& height) {
-        int n = height.size();
-        int ans = 0;
-        std::vector<int> left(n);  // l[i] := max(height[0..i])
-        std::vector<int> right(n); // r[i] := max(height[i..n-1])
-        rightmax(height, right);
-        leftmax(height, left);
-        for (int i = 0; i < n; ++i) {
-            int temp = std::min(right[i], left[i]);
-            if (temp > height[i]) {
-                ans += temp - height[i];
-            }
+    int trap(vector<int>& height) {
+    int n = height.size();
+    int ans = 0;
+    vector<int> left(n);  // l[i] := max(height[0..i])
+    vector<int> right(n);
+    rightmax(height,right);
+    leftmax(height,left);
+    for (int i = 0; i < n; ++i){
+        int temp = min(right[i],left[i]);
+        if((temp - height[i])>0){
+            ans+=temp-height[i];
         }
-        return ans;
+    }
+    return ans;
     }
 };
