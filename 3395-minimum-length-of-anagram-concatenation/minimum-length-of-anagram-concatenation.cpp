325dd8839a7324f24735528390794f7459a5651c
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-05-31 12:54:26 +0530

    Time: 61 ms (50.92%) | Memory: 31.1 MB (40.48%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    int minAnagramLength(string s) {
+    bool ok(string s,int len){
         int n = s.size();
-        vector<int> factos;
-        for(int i=1;i<=sqrt(n);i++) {
-            if(n%i==0) {
-                int k = n/i;
-                if(k!=i) {
-                    factos.push_back(i);
-                    factos.push_back(k);
-                } else {
-                    factos.push_back(i);
+        int first[26] ={0};
+        for(int i =0;i<len;i++){
+            first[s[i]-'a']++;
+        }
+        for(int i=len ;i<n;i+=len){
+            int range[26]={0};
+            for(int j = i ;j<i+len;j++){
+                range[s[j]-'a']++;
+            }
+            for(int j=0;j<26;j++){
+                if(first[j]!=range[j]){
+                    return false;
                 }
             }
         }
-        sort(factos.begin(),factos.end());
-        for(int i=0;i<factos.size();i++) {
-            int ans = factos[i];
-            map<char,int>mp1;
-            for(int i=0;i<ans;i++)
-                mp1[s[i]]++;
-            int f=0;
-            for(int i=ans;i<n;i=i+ans) {
-                map<char,int> mp2;
-                for(int j=i;j<i+ans;j++) {
-                    mp2[s[j]]++;
-                }
-                if(mp1!=mp2) {
-                    f=1;
-                    break;
-                }
+        return true;
+    }
+    int minAnagramLength(string s) {
+        int n = s.size();
+        for(int len = 1;len<=n;len++){
+            if(n%len==0 && ok(s,len)){
+                return len;
             }
-            if(f==0)
-                return ans;
         }
         return n;
     }
