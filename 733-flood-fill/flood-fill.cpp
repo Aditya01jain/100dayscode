57b9776a78fd2123425f1a9e1b466d1bce90ead3
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-07-15 21:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (12.08%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> ans = image;
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int val = image[sr][sc];
+        
+        // If the new color is the same as the original color, no need to do anything
+        if (val == color) return ans;
+
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        ans[sr][sc] = color;
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (auto& dir : directions) {
+                int newX = x + dir.first;
+                int newY = y + dir.second;
+
+                if (newX >= 0 && newX < n && newY >= 0 && newY < m && image[newX][newY] == val) {
+                    ans[newX][newY] = color;
+                    q.push({newX, newY});
+                    image[newX][newY] = color;
+                }
+            }
+        }
+        return ans;
+    }
+};
