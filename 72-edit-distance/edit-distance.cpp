ac584e112e14d940ec4d08c172982f9a6903ae03
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-07-15 16:32:50 +0530

    Time: 4 ms (83.15%) | Memory: 12.9 MB (19.48%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,21 @@
+    class Solution {
+    public:
+        int solve(string &s1,string &s2,int i,int j,vector<vector<int>> &dp){
+            if(i<0) return j+1;
+            if(j<0) return i+1;
+            if(dp[i][j]!=-1){
+                return dp[i][j];
+            } 
+            if(s1[i]==s2[j]){
+                return dp[i][j] = solve(s1,s2,i-1,j-1,dp);
+            }
+            dp[i][j] = min((1+solve(s1,s2,i-1,j,dp)),min((1+solve(s1,s2,i,j-1,dp)),(1+solve(s1,s2,i-1,j-1,dp))));
+            return dp[i][j];
+        }
+        int minDistance(string s1, string s2) {
+            int n = s1.size();
+            int m = s2.size();
+            vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+            return solve(s1,s2,n,m,dp);
+        }
+    };
