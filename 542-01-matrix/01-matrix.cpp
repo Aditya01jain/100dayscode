521fa785b5ccb5d7ae72e580f9b891b4282e412e
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-07-16 21:49:36 +0530

    Time: 42 ms (94.83%) | Memory: 38.3 MB (14.76%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,15 +3,17 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        queue<pair<int, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
+        queue<pair<int, pair<int, int>>> q;
 
         // Initialize the queue with all the cells that are 0
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({i, j});
+                    q.push({0, {i, j}});
+                    ans[i][j] = 0;
+                    vis[i][j] = 1;
                 }
             }
         }
@@ -20,23 +22,24 @@ public:
         vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         while (!q.empty()) {
-            auto [x, y] = q.front();
+            pair<int, pair<int, int>> node = q.front();
             q.pop();
+            int dist = node.first;
+            int x = node.second.first;
+            int y = node.second.second;
 
             for (auto [dx, dy] : directions) {
                 int newX = x + dx;
                 int newY = y + dy;
 
-                // Check bounds and if we find a shorter path to (newX, newY)
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
-                    if (dist[newX][newY] > dist[x][y] + 1) {
-                        dist[newX][newY] = dist[x][y] + 1;
-                        q.push({newX, newY});
-                    }
+                if (newX >= 0 && newX < n && newY >= 0 && newY < m && vis[newX][newY] == 0) {
+                    ans[newX][newY] = dist + 1;
+                    vis[newX][newY] = 1;
+                    q.push({dist + 1, {newX, newY}});
                 }
             }
         }
 
-        return dist;
+        return ans;
     }
 };
