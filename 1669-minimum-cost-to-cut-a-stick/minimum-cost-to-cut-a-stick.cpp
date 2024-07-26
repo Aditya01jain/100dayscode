f93ca677171a867e55732c7ff1555ea4cfc985cb
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-07-26 22:27:26 +0530

    Time: 23 ms (80.52%) | Memory: 12.5 MB (18.55%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -16,7 +16,17 @@ public:
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
         int s = cuts.size();
-        vector<vector<int>> dp(s,vector<int>(s,-1));
-        return solve(cuts,1,s-2,dp);
+        vector<vector<int>> dp(s,vector<int>(s,0));
+        for(int i = s-1;i>0;i--){
+            for(int j = i;j<(s-1);j++){
+                int mini = 1e9;
+                for(int k=i;k<=j;k++){
+                    int res = (cuts[j+1] - cuts[i-1]) + dp[i][k-1] + dp[k+1][j]; 
+                    mini = min(res,mini);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][s-2];
     }
 };
