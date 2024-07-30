dfcba88ce13dc92788f2d9c92b918df3f0a5bc03
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-07-30 23:36:29 +0530

    Time: 8 ms (78.10%) | Memory: 11.1 MB (37.10%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freqMap;
+        for (char c : s) {
+            freqMap[c]++;
+        }
+        priority_queue<pair<int, char>> maxHeap;
+        for (const auto& entry : freqMap) {
+            maxHeap.push({entry.second, entry.first});
+        }
+        string result;
+        while (!maxHeap.empty()) {
+            pair<int,char> temp = maxHeap.top();
+            maxHeap.pop();
+            result.append(temp.first, temp.second); 
+        }
+
+        return result;
+    }
+};
