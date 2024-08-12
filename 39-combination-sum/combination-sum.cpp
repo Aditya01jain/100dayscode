bd97ca9f3ff90c436edf32fed7c29700c39ec0be
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-08-12 12:07:23 +0530

    Time: 1460 ms (5.01%) | Memory: 488.2 MB (5.01%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    void solve(set<vector<int>> &ans,vector<int> out,vector<int>& candidates,int target,int total){
-        if(target==total){
+    void solve(vector<int>& candidates,vector<int>  out,set<vector<int>>& ans,int tar){
+        if(tar==0){
             sort(out.begin(),out.end());
             ans.insert(out);
-            return;
         }
-        if(total>target){
-            return;
-        }
-        for(auto i:candidates){
+        if(tar<0) return ;
+        for(int i:candidates){
             out.push_back(i);
-            solve(ans,out,candidates,target,total+i);
+            solve(candidates,out,ans,tar-i);
             out.pop_back();
         }
-        return ;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         set<vector<int>> ans;
         vector<int> out;
-        int total =0;
-        for(auto i:candidates){
+        for(int i:candidates){
             out.push_back(i);
-            solve(ans,out,candidates,target,total+i);
+            solve(candidates,out,ans,target-i);
             out.pop_back();
         }
-        vector<vector<int>> a(ans.begin(),ans.end());
-        return a;
+        vector<vector<int>> res(ans.begin(),ans.end());
+        return res;
     }
 };
