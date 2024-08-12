01109707f57ca490abe7d63020e22444a63cc91a
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-08-12 22:31:47 +0530

    Time: 75 ms (95.84%) | Memory: 23.9 MB (40.40%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -6,7 +6,6 @@ public:
         int n = h.size();
         int m = h[0].size();
         
-        // Initialize the effort grid with INT_MAX
         vector<vector<int>> eff(n, vector<int>(m, INT_MAX));
         eff[0][0] = 0;
         
@@ -17,8 +16,7 @@ public:
             int x = q.top().second.first;
             int y = q.top().second.second;
             q.pop();
-            
-            // If we have reached the bottom-right corner, return the effort
+
             if (x == n-1 && y == m-1) return diff;
             
             for (auto i : dir) {
@@ -28,7 +26,6 @@ public:
                 if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
                     int newdiff = max(abs(h[x][y] - h[nx][ny]), diff);
                     
-                    // Update the effort if we found a smaller effort path
                     if (newdiff < eff[nx][ny]) {
                         eff[nx][ny] = newdiff;
                         q.push({newdiff, {nx, ny}});
