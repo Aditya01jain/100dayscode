2c88d993027054b43962600131a91eef4ff0cba6
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-08-13 21:08:52 +0530

    Time: 1021 ms (22.34%) | Memory: 12.1 MB (20.37%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    bool searchNext(vector<vector<char>> &board, string word, int row, int col,int index, int m, int n) {
-        if (index == word.length()) return true;
-
-        if (row < 0 || col < 0 || row == m || col == n || board[row][col] != word[index] or board[row][col] == '!')
-            return false;
-
-        char c = board[row][col];
-        board[row][col] = '!';
-
-        bool top = searchNext(board, word, row - 1, col, index + 1, m, n);
-        bool right = searchNext(board, word, row, col + 1, index + 1, m, n);
-        bool bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
-        bool left = searchNext(board, word, row, col - 1, index + 1, m, n);
-
-        board[row][col] = c; 
-        return top || right || bottom || left;
+    bool solve(vector<vector<char>>& board, string word,int i,int j,int n,int m,int in){
+        if(in == word.size()) return true;
+        if(i<0 || j<0 || i==n || j== m || board[i][j]=='!'|| board[i][j]!= word[in]) return false;
+        char c = board[i][j];
+        board[i][j] = '!';
+        bool top = solve(board,word,i-1,j,n,m,in+1);
+        bool bottom = solve(board,word,i+1,j,n,m,in+1);
+        bool left = solve(board,word,i,j-1,n,m,in+1);
+        bool right = solve(board,word,i,j+1,n,m,in+1);
+        board[i][j] = c;
+        return top||bottom||left||right;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0]) {
-                    if (searchNext(board, word, i, j,0, m, n)) return true;
-                }
+        int n = board.size();
+        int m = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]) if(solve(board,word,i,j,n,m,0)) return true;
             }
         }
         return false;
