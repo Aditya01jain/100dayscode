a44d185973cd31883c22408ffa1a0070214ae39f
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-08-22 19:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (5.10%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int countVowelStrings(int n) {
-        vector<int> dict(5, 1);
-        if(n == 0){
-            return 0;
-        }else{
-            for(int i = 1; i <n; ++i){
-                dict[0] = dict[0] + dict[1] + dict[2] + dict[3] + dict[4];
-                dict[1] = dict[1] + dict[2] + dict[3] + dict[4];
-                dict[2] = dict[2] + dict[3] + dict[4];
-                dict[3] = dict[3] + dict[4];
-                dict[4] = dict[4];
-            }
-        }
-        return dict[0] + dict[1] + dict[2] + dict[3] + dict[4];
 
+    int solve(string str, int ind,vector<string>&m,int len, int n, 
+    vector<vector<int>>&dp)
+    {
+        if(len==n)
+        {
+            return 1;
+        }
+        if(ind==5)
+        {
+            return 0;
+        }
+        if(dp[len][ind]!=-1) return dp[len][ind];
+        int np = solve(str,ind+1,m,len,n,dp);
+
+        int p = 0;
+        if(len<n)
+        {
+            str = str+m[ind];
+            p = solve(str,ind,m,len+1,n,dp);
+        }
+        
+        return dp[len][ind] = np+p;
+    }
+    int countVowelStrings(int n) {
+        vector<string>m = {"a","e","i","o","u"};
+        string str = "";
+        vector<vector<int>>dp(n,vector<int>(5,-1));
+        int ans = solve(str,0,m,0,n,dp);
+        return ans;
     }
 };
