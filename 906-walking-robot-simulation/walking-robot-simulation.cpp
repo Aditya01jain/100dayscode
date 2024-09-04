aaa1090012978f123fd8dc995f3108109cc98f88
ADITYA JAIN <118585917+Aditya01jain@users.noreply.github.com>
2024-09-04 12:13:35 +0530

    Time: 74 ms (63.81%) | Memory: 37.3 MB (86.19%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,25 +1,11 @@
-struct VectorHash {
-    size_t operator()(const std::vector<int>& v) const {
-        std::hash<int> hasher;
-        size_t seed = 0;
-        for (int i : v) {
-            seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        }
-        return seed;
-    }
-};
 class Solution {
 public:
-    bool find(vector<int> p, vector<vector<int>>& ob) {
-        for (auto i : ob)
-            if (p == i)
-                return false;
-        return true;
-    }
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
         vector<int> p = {0, 0};
-        unordered_map<vector<int>, int, VectorHash> mp;
-        for(auto i:obstacles) mp[i]++;
+        set<pair<int, int>> obstacleSet;
+        for (auto& obs : obstacles) {
+            obstacleSet.insert({obs[0], obs[1]});
+        }
         int ans = 0;
         char dir = 'N';
         for (int i : commands) {
@@ -35,47 +21,44 @@ public:
                 else if (dir == 'E') dir = 'S';
                 else if (dir == 'W') dir = 'N';
             }
-            else{
-                if (dir == 'N'){
-                    for(int j =1;j<=i;j++){
-                        p[1]+=1;
-                        if(mp[p]==1){
-                            p[1]-=1;
+            else {
+                if (dir == 'N') {
+                    for (int j = 1; j <= i; j++) {
+                        p[1] += 1;
+                        if (obstacleSet.count({p[0], p[1]})) {
+                            p[1] -= 1;
                             break;
                         }
                     }
-                    ans = max(ans,(p[0]*p[0] + p[1]*p[1]));
                 }
-                else if (dir == 'S'){
-                    for(int j =1;j<=i;j++){
-                        p[1]-=1;
-                        if(mp[p]==1){
-                            p[1]+=1;
+                else if (dir == 'S') {
+                    for (int j = 1; j <= i; j++) {
+                        p[1] -= 1;
+                        if (obstacleSet.count({p[0], p[1]})) {
+                            p[1] += 1;
                             break;
                         }
                     }
-                    ans = max(ans,(p[0]*p[0] + p[1]*p[1]));
                 }
-                else if (dir == 'E'){
-                    for(int j =1;j<=i;j++){
-                        p[0]+=1;
-                        if(mp[p]==1){
-                            p[0]-=1;
+                else if (dir == 'E') {
+                    for (int j = 1; j <= i; j++) {
+                        p[0] += 1;
+                        if (obstacleSet.count({p[0], p[1]})) {
+                            p[0] -= 1;
                             break;
                         }
                     }
-                    ans = max(ans,(p[0]*p[0] + p[1]*p[1]));
                 }
-                else if (dir == 'W'){
-                    for(int j =1;j<=i;j++){
-                        p[0]-=1;
-                        if(mp[p]==1){
-                            p[0]+=1;
+                else if (dir == 'W') {
+                    for (int j = 1; j <= i; j++) {
+                        p[0] -= 1;
+                        if (obstacleSet.count({p[0], p[1]})) {
+                            p[0] += 1;
                             break;
                         }
                     }
-                    ans = max(ans,(p[0]*p[0] + p[1]*p[1]));
                 }
+                ans = max(ans, p[0] * p[0] + p[1] * p[1]);
             }
         }
         return ans;
